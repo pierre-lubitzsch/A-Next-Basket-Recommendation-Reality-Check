fix bug not loading decoder and disable cuDNN for hvp calculation because it is not supported for RNNs...

methods/sets2sets/scif.py
@@ -108,10 +108,14 @@ def _hvp_single(encoder, decoder, inp, tgt, v_list, param_list,
                 codes_inverse_freq, criterion, output_size, max_len):
     loss = _loss_forward(encoder, decoder, inp, tgt,
                          codes_inverse_freq, criterion, output_size, max_len)
-    grad = torch.autograd.grad(loss, param_list, create_graph=True)
-    dot  = sum((g * v).sum() for g, v in zip(grad, v_list))
-    hv   = torch.autograd.grad(dot, param_list, retain_graph=False)
+    
+    # second derivative not supported for RNNs when using cuDNN...
+    with torch.backends.cudnn.flags(enabled=False):
+        grad = torch.autograd.grad(loss, param_list, create_graph=True)
+        dot  = sum((g * v).sum() for g, v in zip(grad, v_list))
+        hv   = torch.autograd.grad(dot, param_list, retain_graph=False)
     return [h.detach() for h in hv]
+
 
 def _hvp_dataset(
     encoder, decoder, data_batch,

methods/sets2sets/unlearn_sets2sets.py
@@ -761,7 +761,7 @@ def unlearn_main():
         encoder_instance = torch.load(encoder_pathes, map_location=torch.device('cuda' if use_cuda else 'cpu'), weights_only=False)
         decoder_instance = torch.load(decoder_pathes, map_location=torch.device('cuda' if use_cuda else 'cpu'), weights_only=False)
 
-        unlearnIters(history_data, future_data, input_size, encoder_instance, attn_decoder, model_version, training_key_set, val_key_set, retain_key_set, weights,
+        unlearnIters(history_data, future_data, input_size, encoder_instance, decoder_instance, model_version, training_key_set, val_key_set, retain_key_set, weights,
                    next_k_step, num_iter, topk, seed, temporal_split, LOCAL, user_to_unlearning_items, unlearning_algorithm, args=args)
     
     encs, decs = _find_trained_models(args, model_version, seed)